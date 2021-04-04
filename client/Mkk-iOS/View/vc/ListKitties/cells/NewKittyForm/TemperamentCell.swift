163be38f9c23644a406f597db1f3095e642d1c2d
lozzoc <reach.maddaloc@gmail.com>
2021-04-04 15:47:07 -0400

    fixed issue with saving to database and deleting stale notifications

---
+++ client/Mkk-iOS/View/vc/ListKitties/cells/NewKittyForm/TemperamentCell.swift
@@ -0,0 +1,23 @@
+//
+//  TemperamentCell.swift
+//  Mkk-iOS
+//
+//  Created by Conner M on 4/4/21.
+//
+
+import UIKit
+
+class TemperamentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
