38ca46491fb639cace5f7865ebbe0c3bf9454c8c
lozzoc <reach.maddaloc@gmail.com>
2021-04-03 21:27:31 -0400

    upgraded to prod url

--- client/Mkk-iOS/Model/Strings.swift
+++ client/Mkk-iOS/Model/Strings.swift
@@ -29,9 +29,9 @@ let KITTY_BREEDS: [String]  = [
 
 
 let VOTE_CELL_REUSE:String = "This cell has a picture and a thick segmented control."
-//let SERVER_URL = "https://kissmarrykill.herokuapp.com"
+let SERVER_URL = "https://kissmarrykill.herokuapp.com"
 //let SERVER_URL = "http://localhost:3000"
-let SERVER_URL = "http://10.1.10.74:3000"
+//let SERVER_URL = "http://10.1.10.74:3000"
 
 
 enum KMKNetworkError: String,Error{
